c751b25ba6820fe7dbc785055626fe8b504bfbe7
Kazuaki Tanaka <kazuaki@ics.kyutech.ac.jp>
2024-01-14 22:09:43 +0900

    Change folder name

--- program01/main.cpp
+++ program01/main.cpp
@@ -4,22 +4,34 @@
 #include <opencv2/highgui.hpp>
 
 
+void display(cv::Mat image, std::string windowName = "Image") {
+    cv::namedWindow(windowName);
+    cv::imshow(windowName, image);
+    cv::waitKey(1000 * 100);
+    cv::destroyWindow(windowName);
+}
+
 int main(int argc, char* argv[]){
 
-  cv::Mat img = cv::imread("circle.png");
+  cv::Mat img = cv::imread("road.jpg", 0);
+
+  display(img);
 
   int x_size = img.cols;
   int y_size = img.rows ;
   int i, j;
-  int count = 0;
+
+  cv::Mat output = cv::Mat::zeros(y_size, x_size, CV_8UC1);
 
   for( i=0 ; i<x_size ; i++ ){
     for( j=0 ; j<y_size ; j++ ){
-      if( img.at<uchar>(j, i) == 0 ) count++;
+      if( img.at<uchar>(j, i) > 160 ){
+        output.at<uchar>(j, i) = 255;
+      };
     }
   }
 
-  std::cout << "S=" << count << std::endl;
+  display(output);
     
   return 0;
 }
--- program02/main.cpp
+++ program02/main.cpp
@@ -4,34 +4,22 @@
 #include <opencv2/highgui.hpp>
 
 
-void display(cv::Mat image, std::string windowName = "Image") {
-    cv::namedWindow(windowName);
-    cv::imshow(windowName, image);
-    cv::waitKey(1000 * 100);
-    cv::destroyWindow(windowName);
-}
-
 int main(int argc, char* argv[]){
 
-  cv::Mat img = cv::imread("road.jpg", 0);
-
-  display(img);
+  cv::Mat img = cv::imread("circle.png");
 
   int x_size = img.cols;
   int y_size = img.rows ;
   int i, j;
-
-  cv::Mat output = cv::Mat::zeros(y_size, x_size, CV_8UC1);
+  int count = 0;
 
   for( i=0 ; i<x_size ; i++ ){
     for( j=0 ; j<y_size ; j++ ){
-      if( img.at<uchar>(j, i) > 160 ){
-        output.at<uchar>(j, i) = 255;
-      };
+      if( img.at<uchar>(j, i) == 0 ) count++;
     }
   }
 
-  display(output);
+  std::cout << "S=" << count << std::endl;
     
   return 0;
 }
